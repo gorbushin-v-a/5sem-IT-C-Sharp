24296ee60cbb967931c85c3b1d18083605b05e07
gorbushin-v-a <gorbushin.volodja@mail.ru>
2021-03-15 22:04:36 +0300

    8

--- Csh_7/Csh_7/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Csh_7/Csh_7/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -141,7 +141,7 @@ namespace Csh_7 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Csh_7;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Csh_7;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
